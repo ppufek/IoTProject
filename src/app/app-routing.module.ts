9512795a35de78f4614356a8428c4c1ec9b120fd
Dominik Lovric <dxl4589@rit.edu>
2020-03-04 23:56:19 +0100

    logo added on landing and login page, login form css added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LandingComponent } from './landing/landing.component';
+
 
 
 const routes: Routes = [ 
-  {path: '', component: LoginComponent },
+  {path: '', component: LandingComponent },
+  {path: 'dominik', component: LoginComponent },
   {path: 'paula', component: DashboardComponent}
 ];
 
