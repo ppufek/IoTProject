4100ab78f3ac82f976015bcfe84ed8fc82f6bed2
Dominik Lovric <dxl4589@g.rit.edu>
2020-04-17 22:24:33 +0200

    table view added for light and temperature measurements

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,33 +3,11 @@ import { SharedService } from '../shared.service';
 import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  date: string;
+  value: string;
+  measurementUnit: string;
 }
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
+
 
 @Component({
   selector: 'app-dashboard',
@@ -39,22 +17,42 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class DashboardComponent implements OnInit {
 
 
-  displayedColumns: string[] = ['position', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['date', 'value', 'measurementUnit'];
+  dataSource = new MatTableDataSource<PeriodicElement>();
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  // message: Map<string, string[]>
+  message: Map<string, string[]>
   // message1: Map<string, string[]>
 
-  constructor(private sharedService: SharedService) {}
+  constructor(private sharedService: SharedService) { }
 
   ngOnInit(): void {
     this.dataSource.paginator = this.paginator;
 
-    // this.sharedService.sharedMessage1.subscribe(message => this.message = message)
-    // console.log("Hello from Dashb TEMP")
-    // console.log(this.message)
+    this.sharedService.sharedMessage1.subscribe(message => this.message = message)
+
+    let newArray: PeriodicElement[] = []
+
+    for (let key of Array.from(this.message.keys())) {
+
+      for (let i = 0; i < this.message.get(key).length; i++) {
+        if (key === "valuesTemp") {
+          newArray.forEach(obj => {
+              obj.value = this.message.get(key)[i];
+          });
+        } else {
+          let modal: PeriodicElement = {
+            date: this.message.get(key)[i],
+            value: "0",
+            measurementUnit: "C"
+          };
+          newArray.push(modal)
+        }
+      }
+    }
+
+    this.dataSource = new MatTableDataSource<PeriodicElement>(newArray);
 
     // this.sharedService.sharedMessage2.subscribe(message => this.message1 = message)
     // console.log("Hello from Dashb LIGHT")
--- src/app/light-measurement/light-measurement.component.ts
+++ src/app/light-measurement/light-measurement.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import { SharedService } from '../shared.service';
 import HC_exporting from 'highcharts/modules/exporting';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+
+export interface PeriodicElement {
+    date: string;
+    value: string;
+    measurementUnit: string;
+  }
 
 @Component({
   selector: 'app-light-measurement',
@@ -10,12 +17,46 @@ import HC_exporting from 'highcharts/modules/exporting';
 })
 export class LightMeasurementComponent implements OnInit {
 
+    displayedColumns: string[] = ['date', 'value', 'measurementUnit'];
+    dataSource = new MatTableDataSource<PeriodicElement>();
   
+    @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  
+  message1: Map<string, string[]>
+
+
   chartsOptions: {};
   Highcharts = Highcharts;
   constructor(private sharedService: SharedService) { }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+
+    this.sharedService.sharedMessage2.subscribe(message => this.message1 = message)
+    console.log( "ovdje", this.message1); 
+
+    let newArray: PeriodicElement[] = []
+
+    for (let key of Array.from(this.message1.keys())) {
+
+      for (let i = 0; i < this.message1.get(key).length; i++) {
+        if (key === "valuesLight") {
+          newArray.forEach(obj => {
+              obj.value = this.message1.get(key)[i];
+          });
+        } else {
+          let modal: PeriodicElement = {
+            date: this.message1.get(key)[i],
+            value: "0",
+            measurementUnit: "lx"
+          };
+          newArray.push(modal)
+        }
+      }
+    }
+    this.dataSource = new MatTableDataSource<PeriodicElement>(newArray);
+
+
       this.sharedService.sharedMessage2.subscribe(measurements => {
           console.log('Widget', measurements);
 
@@ -71,6 +112,7 @@ export class LightMeasurementComponent implements OnInit {
 
       })
 
+     
 
       HC_exporting(Highcharts);
 
